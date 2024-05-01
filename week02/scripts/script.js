fd6069aca6c423b72613c7ab51c050fefe5bdae8
wdd230 <rob23047@byui.edu>
2024-05-01 14:57:46 -0600

    W02

---
+++ week02/scripts/script.js
@@ -0,0 +1,23 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+    if (input.value != '') {
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        });
+    } else {
+        input.value ='';
+        input.focus();
+        return;
+    }
+});
