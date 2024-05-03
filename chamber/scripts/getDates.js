1a7af49c1c7b6de68076282730344d78e9fef8bc
wdd230 <rob23047@byui.edu>
2024-05-03 17:04:43 -0600

    milestone

---
+++ chamber/scripts/getDates.js
@@ -0,0 +1,16 @@
+/* Copyright year */
+
+function getCopyrightYear() {
+    const year = new Date().getFullYear();
+    return `&copy; ${year}`;
+}
+document.getElementById("currentYear").innerHTML = getCopyrightYear();
+
+/* Last modified date of the page */
+
+function getLastModified() {
+    const lastModified = new Date(document.lastModified).toGMTString();
+    return `Last Modified: ${lastModified}`;
+}
+document.getElementById("lastModified").innerHTML = getLastModified();
+
