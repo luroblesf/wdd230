b67d28f06876348772e00a07b11faafe03b4e310
wdd230 <rob23047@byui.edu>
2024-05-04 10:19:39 -0600

    milestone

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -14,3 +14,12 @@ function getLastModified() {
 }
 document.getElementById("lastModified").innerHTML = getLastModified();
 
+/* Hamburger Menu */
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+});
