65cf0a1beffcf020b524614f5cf740d471deea4a
wdd230 <rob23047@byui.edu>
2024-05-18 09:37:44 -0600

    milestone w04

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -24,6 +24,26 @@ hamButton.addEventListener("click", () => {
     hamButton.classList.toggle("open");
 });
 
+/* Title Position */
+const form = document.querySelector("#form");
+const input = document.querySelector("#position");
+const output = document.querySelector("#submit");
+
+const re = /^[A-Za-z\d\-\s]{7,}$/;
+
+function testInfo(positionInput) {
+  const ok = re.exec(positionInput.value);
+
+  output.textContent = ok
+    ? {onclick}
+    : `${positionInput.value} Title of position invalid`;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  testInfo(input);
+});
+
 /* TimeStamp */
 
 var now = new Date();
